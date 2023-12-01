c40542bc770d0fd47b6e1845bc60ee576ef3b10a
da0293 <dy7133@naver.com>
2023-12-02 01:10:43 +0900

    commit 5. 특정 문자 뒤집기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,29 +1,31 @@
 package practice;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public ArrayList<String> solution(String[] s) {
-		ArrayList<String> answer = new ArrayList<>();
-		for(String x : s ) {
-			String tmp = new StringBuilder(x).reverse().toString();
-			answer.add(tmp);
+	public String solution(String str) {
+		String answer = "";
+		char[] ch = str.toCharArray();
+		int lt = 0; int rt = str.length()-1;
+		while(lt<rt) {
+			if(!Character.isAlphabetic(ch[lt]))lt++;
+			else if (!Character.isAlphabetic(ch[rt]))rt--;
+			else {
+				char temp = ch[lt]; 
+				ch[lt] = ch[rt]; 
+				ch[rt] = temp;
+				lt++; 
+				rt--;  
+			}
 		}
-	
+		answer = String.valueOf(ch);
 		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String[] s = new String[n]; 
-		for( int i = 0; i < n ; i++ ) {
-			s[i] = sc.next();
-		}
-		for( String x : T.solution(s)) {
-			System.out.println(x);
-		}
+		String str = sc.next();
+		System.out.println(T.solution(str));
 	}
 }
