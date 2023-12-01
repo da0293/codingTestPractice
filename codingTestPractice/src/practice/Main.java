f8d7570ba5b3b994e177e76307525a3b26dd53f7
da0293 <dy7133@naver.com>
2023-12-02 01:40:24 +0900

    commit 9. 숫자만 추출

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -3,18 +3,20 @@ package practice;
 import java.util.Scanner;
 
 public class Main {
-	public String solution(String str) {
-		String answer = "NO";
-		str =  str.toUpperCase().replaceAll("[^A-Z]", "");
-		String temp = new StringBuffer(str).reverse().toString();
-		if (str.equals(temp)) answer = "YES";
+	public int solution(String str) {
+		int answer = 0; 
+		String temp = ""; 
+		for( char x : str.toCharArray()) {
+			if( Character.isDigit(x)) temp+=x;  
+		}
+		answer = Integer.parseInt(temp); 
 		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
+		String str = sc.next();
 		System.out.println(T.solution(str));
 	}
 }
