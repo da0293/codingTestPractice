1baa592e5683ed28d5aca30701e59aadb24c6c4c
da0293 <dy7133@naver.com>
2023-12-01 20:26:29 +0900

    commit 1-3. 문장 속 단어

---
+++ codingTestPractice/src/practice/Main.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main {
+	public String solution(String str) {
+		String answer = "";
+		String[] s = str.split(" ");
+		int m = Integer.MIN_VALUE;
+		for( String x : s ) {
+			int len = x.length();
+			if ( len > m ) {
+				m = len; 
+				answer = x; 
+			}
+		}
+		return answer; 
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.print(T.solution(str));
+	}
+}
