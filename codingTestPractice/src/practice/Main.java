4020bcccca734f967b0e0776caa3ede3950e227e
Sda0293 <dy7133@naver.com>
2023-12-02 00:50:51 +0900

    commit 4. 단어 뒤집기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,26 +1,29 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public String solution(String str) {
-		String answer = "";
-		String[] s = str.split(" ");
-		int m = Integer.MIN_VALUE;
-		for( String x : s ) {
-			int len = x.length();
-			if ( len > m ) {
-				m = len; 
-				answer = x; 
-			}
+	public ArrayList<String> solution(String[] s) {
+		ArrayList<String> answer = new ArrayList<>();
+		for(String x : s ) {
+			String tmp = new StringBuilder(x).reverse().toString();
+			answer.add(tmp);
 		}
+	
 		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.print(T.solution(str));
+		int n = sc.nextInt();
+		String[] s = new String[n]; 
+		for( int i = 0; i < n ; i++ ) {
+			s[i] = sc.next();
+		}
+		for( String x : T.solution(s)) {
+			System.out.println(x);
+		}
 	}
 }
