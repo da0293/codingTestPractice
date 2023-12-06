913c387d76afed3a581d112a3a67027e9107ff40
da0293 <dy7133@naver.com>
2023-12-06 20:22:22 +0900

    commit 12. 암호

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,30 +2,24 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public String solution(String str){
+	public String solution(int n, String str){
 		String answer = "" ;
-		int cnt = 1; 
-		for( int i = 0; i < str.length()-1; i++) {
-			// 현재문자와 다음 문자가 같을 경우 1증가
-			if (str.charAt(i) == str.charAt(i+1)) {
-				cnt++; 
-			} else {
-				// 현재 문자와 다음 문자가 다를 경우 answer에 추가 
-				answer += str.charAt(i);
-				if( cnt > 1 ) answer += cnt; 
-				cnt = 1; 
-			}
+		String temp = ""; 
+		int t = 0; 
+		for ( int i = 0; i < n; i++) {
+			temp = str.substring(0,7). replace("#", "1").replace("*", "0"); 
+			t = Integer.parseInt(temp,2); 
+			answer += (char)(t); 
+			str = str.substring(7); 
 		}
-		// 마지막 문자 
-		answer += str.charAt(str.length()-1);
-		if( cnt > 1 ) answer+=cnt;
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		String str = sc.next();
-		System.out.println(T.solution(str));
+		System.out.println(T.solution(n, str));
 	}
 }
