83930715041bbfd8d9658a717dca5d5282926a83
da0293 <dy7133@naver.com>
2023-12-06 20:43:03 +0900

    import java.util.Scanner;

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,24 +2,23 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public String solution(int n, String str){
-		String answer = "" ;
-		String temp = ""; 
-		int t = 0; 
-		for ( int i = 0; i < n; i++) {
-			temp = str.substring(0,7). replace("#", "1").replace("*", "0"); 
-			t = Integer.parseInt(temp,2); 
-			answer += (char)(t); 
-			str = str.substring(7); 
+	public String solution(String str){
+		String answer = ""; 
+		String[] s = str.split(" ");
+		int min = Integer.MIN_VALUE;
+		for( String x : s) {
+			if ( x.length() > min) {
+				min = x.length();
+				answer = x; 
+			}
 		}
-		return answer;
+		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String str = sc.next();
-		System.out.println(T.solution(n, str));
+		String str =  sc.nextLine();
+		System.out.println(T.solution(str));
 	}
 }
