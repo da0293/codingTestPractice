79f45c22e5667bcb2b6c91cc47449c85648fbd2a
da0293 <dy7133@naver.com>
2023-12-06 23:22:54 +0900

    commit 8. 유효한 팰린드롬

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -4,26 +4,16 @@ import java.util.Scanner;
 public class Main {
 	public String solution(String str){
 		String answer = "YES"; 
-		str = str.toUpperCase();
-		int lt = 0; 
-		int rt = str.length()-1; 
-		while(lt<rt) {
-			if(str.charAt(lt) == str.charAt(rt)) {
-				lt++; 
-				rt--; 
-			}else {
-				answer = "NO";
-				break; 
-			}
-		}
-		
+		str = str.toUpperCase().replaceAll("[^A-Z]", "");
+		String temp = new StringBuffer(str).reverse().toString();
+		if(!str.equals(temp))answer="NO"; 
 		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
+		String s = sc.nextLine();
 		System.out.println(T.solution(s));
 	}
 }
