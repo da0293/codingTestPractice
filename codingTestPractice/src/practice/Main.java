cd8201a16912040754769db80e897f6eb1c8bbed
da0293 <dy7133@naver.com>
2023-12-11 23:52:58 +0900

    commit 12. 암호

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,28 +2,25 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public String solution(String str){
+	public String solution(int n, String str){
 		String answer = ""; 
-		char[] c =  str.toCharArray();
-		int cnt = 1; 
-		for( int i = 0; i < str.length()-1; i++ ) {
-			if (c[i] == c[i+1]) cnt++;
-			else {
-				answer += c[i]; 
-				if( cnt > 1 ) answer += cnt; 
-				cnt = 1; 
-			}
+		String temp = "";
+		int x = 0; 
+		for(int i = 0; i < n; i++) {
+			temp = str.substring(0,7).replace("#", "1").replace("*", "0"); ;
+			x = Integer.parseInt(temp,2); 
+			answer += (char)(x); 
+			str =  str.substring(7); 
 		}
-		answer += c[str.length()-1];  
-		if( cnt > 1) answer+= cnt; 
 		return answer; 
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
+		int n =  sc.nextInt();
 		String str = sc.next();
-		System.out.println(T.solution(str));
+		System.out.println(T.solution(n,str));
 
 	}
 }
