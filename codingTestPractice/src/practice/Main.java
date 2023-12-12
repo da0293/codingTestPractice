3913fb50cf2fed2506c0349af9632174888d0ed8
da0293 <dy7133@naver.com>
2023-12-12 23:10:15 +0900

    part2 commit 1. 큰 수 출력하기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,16 +1,15 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-	public String solution(int n, String str){
-		String answer = ""; 
-		String temp = "";
-		int x = 0; 
-		for(int i = 0; i < n; i++) {
-			temp = str.substring(0,7).replace("#", "1").replace("*", "0"); ;
-			x = Integer.parseInt(temp,2); 
-			answer += (char)(x); 
-			str =  str.substring(7); 
+	public ArrayList<Integer> solution(int n, int[] numArray){
+		ArrayList<Integer> answer = new ArrayList<>(); 
+		answer.add(numArray[0]); 
+		for( int i = 1; i < n; i++ ) {
+			if(numArray[i-1] < numArray[i]) {
+				answer.add(numArray[i]); 
+			}
 		}
 		return answer; 
 	}
@@ -19,8 +18,13 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n =  sc.nextInt();
-		String str = sc.next();
-		System.out.println(T.solution(n,str));
+		int[] numArray = new int[n];
+		for(int i = 0; i < n; i++) {
+			numArray[i] = sc.nextInt();
+		}
 
+		for(int answer : T.solution(n,numArray)){
+			System.out.print(answer +" ");
+		}
 	}
 }
