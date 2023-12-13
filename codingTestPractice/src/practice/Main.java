970b2c4f22b647c2b8478c7409e6c328ddf8dbdd
da0293 <dy7133@naver.com>
2023-12-13 23:38:48 +0900

    part2 3. 가위 바위 보

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,14 +2,14 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public int solution(int n, int[] numArray){
-		int answer = 1;  
-		int max = numArray[0]; 
-		for( int i = 0; i < numArray.length; i++) {
-			if( numArray[i] > max) {
-				answer++; 
-				max = numArray[i]; 
-			}
+	public String solution(int n, int[] a, int[] b){
+		String answer = ""; 
+		for( int i=0; i< n; i++) {
+			if(a[i] == b[i]) answer += "D"; 
+			else if(a[i]==2 && b[i]==1) answer += "A"; 
+			else if(a[i]==3 && b[i]==2) answer += "A"; 
+			else if(a[i]==1 && b[i]==3) answer += "A"; 
+			else answer+= "B"; 
 		}
 		return answer; 
 	}
@@ -18,10 +18,16 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n =  sc.nextInt();
-		int[] numArray = new int[n];
-		for(int i = 0; i < n; i++) {
-			numArray[i] = sc.nextInt();
+		int[] a = new int[n];  
+		int[] b =  new int[n];  
+		for( int i = 0 ; i < n; i++ ) {
+			a[i] = sc.nextInt();
 		}
-		System.out.println(T.solution(n,numArray));
+		for( int i = 0 ; i < n; i++ ) {
+			b[i] = sc.nextInt();
+		}
+		for( char x : T.solution(n, a, b).toCharArray()) {
+			System.out.println(x);
+		}
 	}
 }
