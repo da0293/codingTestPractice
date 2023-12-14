9b635d742e13593482b3187a70d0f7c7213fa5af
da0293 <dy7133@naver.com>
2023-12-14 17:45:58 +0900

    commit part2. 4. 피보나치 수열

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,14 +2,12 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public String solution(int n, int[] a, int[] b){
-		String answer = ""; 
-		for( int i=0; i< n; i++) {
-			if(a[i] == b[i]) answer += "D"; 
-			else if(a[i]==2 && b[i]==1) answer += "A"; 
-			else if(a[i]==3 && b[i]==2) answer += "A"; 
-			else if(a[i]==1 && b[i]==3) answer += "A"; 
-			else answer+= "B"; 
+	public int[] solution(int n){
+		int[] answer = new int[n]; 
+		answer[0] = 1; 
+		answer[1] = 1; 
+		for( int i =2; i < n; i++) {
+			answer[i] = answer[i-1] + answer[i-2]; 
 		}
 		return answer; 
 	}
@@ -17,17 +15,9 @@ public class Main {
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n =  sc.nextInt();
-		int[] a = new int[n];  
-		int[] b =  new int[n];  
-		for( int i = 0 ; i < n; i++ ) {
-			a[i] = sc.nextInt();
-		}
-		for( int i = 0 ; i < n; i++ ) {
-			b[i] = sc.nextInt();
-		}
-		for( char x : T.solution(n, a, b).toCharArray()) {
-			System.out.println(x);
+		int n = sc.nextInt();
+		for( int x : T.solution(n)) {
+			System.out.print(x + " ");
 		}
 	}
 }
