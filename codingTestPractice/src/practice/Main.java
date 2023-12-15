a40d26a0a3474308e6338822a097337afb23c07b
da0293 <dy7133@naver.com>
2023-12-15 19:55:17 +0900

    commit 5. 소수(에라토스테네스 체)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,22 +2,24 @@ package practice;
 
 import java.util.Scanner;
 public class Main {
-	public int[] solution(int n){
-		int[] answer = new int[n]; 
-		answer[0] = 1; 
-		answer[1] = 1; 
-		for( int i =2; i < n; i++) {
-			answer[i] = answer[i-1] + answer[i-2]; 
+	public int solution(int n){
+		int answer = 0;
+		// 임의의 ck배열을 n+1 배열로 만드는건 인덱스번호를 자연수라고 여김
+		int[] ck = new int[n+1];
+		for( int i=2; i<=n; i++) {
+			if( ck[i]==0 ) {
+				answer++; 
+				// i의 배수로 for문을 돈다.
+				for( int x=i; x<=n; x=x+i) ck[x]=1; 
+			}
 		}
-		return answer; 
+		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for( int x : T.solution(n)) {
-			System.out.print(x + " ");
-		}
+		System.out.print(T.solution(n));
 	}
 }
