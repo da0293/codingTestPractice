0bc25f97d7e48315451cb5e7fd91e15f73c0be13
da0293 <dy7133@naver.com>
2023-12-15 20:50:36 +0900

    commit 6. 뒤집은 소수

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,25 +1,42 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-	public int solution(int n){
-		int answer = 0;
-		// 임의의 ck배열을 n+1 배열로 만드는건 인덱스번호를 자연수라고 여김
-		int[] ck = new int[n+1];
-		for( int i=2; i<=n; i++) {
-			if( ck[i]==0 ) {
-				answer++; 
-				// i의 배수로 for문을 돈다.
-				for( int x=i; x<=n; x=x+i) ck[x]=1; 
+	public ArrayList<Integer> solution(int n, int[] arr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		int[] ck = new int[n]; 
+		for( int i=0; i<n; i++ ) {
+			int temp=arr[i]; 
+			int reverse=0; 
+			while(temp>0) {
+				int t=temp%10; 
+				reverse=reverse*10+t; 
+				temp=temp/10; 
 			}
+			if(isPrime(reverse)) answer.add(reverse); 
 		}
 		return answer;
 	}
 	
+	private boolean isPrime(int reverse) {
+		if(reverse==1) return false;
+		for( int i=2; i<reverse; i++) {
+			if(reverse%i==0) return false; 
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.print(T.solution(n));
+		int[] arr = new int[n];
+		for( int i=0; i<n; i++ ) {
+			arr[i]=sc.nextInt(); 
+		}
+		for(int x : T.solution(n,arr)) {
+			System.out.print(x + " ");
+		}
 	}
 }
