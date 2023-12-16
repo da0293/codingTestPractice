d77e751f3e1ec437dbd1b1f0bb50765874b76796
da0293 <dy7133@naver.com>
2023-12-16 23:51:10 +0900

    commit 7. 점수계산

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,42 +1,29 @@
 package practice;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
-	public ArrayList<Integer> solution(int n, int[] arr){
-		ArrayList<Integer> answer = new ArrayList<>();
-		int[] ck = new int[n]; 
-		for( int i=0; i<n; i++ ) {
-			int temp=arr[i]; 
-			int reverse=0; 
-			while(temp>0) {
-				int t=temp%10; 
-				reverse=reverse*10+t; 
-				temp=temp/10; 
+	public int solution(int n, int []arr){
+		int answer = 0;
+		int cnt=1; 
+		for( int i = 0; i<n; i++) {
+			if(arr[i]==0) cnt = 1;
+			else {
+				answer+= cnt;
+				cnt++;
 			}
-			if(isPrime(reverse)) answer.add(reverse); 
 		}
 		return answer;
 	}
 	
-	private boolean isPrime(int reverse) {
-		if(reverse==1) return false;
-		for( int i=2; i<reverse; i++) {
-			if(reverse%i==0) return false; 
-		}
-		return true;
-	}
-
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for( int i=0; i<n; i++ ) {
-			arr[i]=sc.nextInt(); 
+		int arr[] = new int[n];
+		for( int i=0; i< n; i++) {
+			arr[i] = sc.nextInt();
 		}
-		for(int x : T.solution(n,arr)) {
-			System.out.print(x + " ");
-		}
+		System.out.print(T.solution(n, arr));
 	}
 }
