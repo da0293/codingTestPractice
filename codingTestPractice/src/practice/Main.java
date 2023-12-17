1c1be020d75bae67f7f5128a7b9f19200b14cd85
da0293 <dy7133@naver.com>
2023-12-17 13:40:36 +0900

    commit 7. 점수계산

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 	public int solution(int n, int []arr){
-		int answer = 0;
-		int cnt=1; 
-		for( int i = 0; i<n; i++) {
-			if(arr[i]==0) cnt = 1;
+		int answer=0;
+		int cnt=0; 
+		for( int i=0; i<arr.length; i++) {
+			if( arr[i]==1) cnt++; 
 			else {
-				answer+= cnt;
-				cnt++;
+				cnt=0; 
 			}
+			answer+=cnt; 
 		}
 		return answer;
 	}
