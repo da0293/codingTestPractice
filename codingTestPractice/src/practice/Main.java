9be15dcc150829775df714c98ae2e229ca183f62
da0293 <dy7133@naver.com>
2023-12-17 17:33:39 +0900

    commit 9. 격자판 최대합

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -3,40 +3,46 @@ package practice;
 import java.util.Scanner;
 
 public class Main {
-	public int solution(int n, int[][] arr){
-		int answer = 0;
-		int temp=0; 
-		int sum1, sum2; 
-		for( int y=0; y<n; y++ ) {
-			sum1=0; sum2=0; 
-			for( int x=0; x<n; x++) {
-				sum1+=arr[y][x]; 
-				sum2+=arr[x][y]; 
-			}
-			temp=Math.max(sum1,sum2); 
-			if(temp>answer)answer=temp; 
-		}
-		sum1=0; sum2=0; 
-		temp=0; 
-		for( int i=0; i<n; i++ ) {
-			sum1+=arr[i][i]; 
-			sum2+=arr[n-1-i][i]; 
-		}
-		temp=Math.max(sum1, sum2); 
-		if(temp>answer)answer=temp; 
-		return answer;
-	}
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        
+        int[] rowSum = new int[n]; 
+        int[] colSum = new int[n]; 
+        int diagonalSum1=0, diagonalSum2=0; 
+        
+        for( int y=0; y<n; y++) {
+        	for( int x=0; x<n; x++) {
+        		rowSum[y]+=arr[y][x];
+        		colSum[x]+=arr[x][y]; 
+        		
+        		if(y==x) {
+        			diagonalSum1+=arr[y][x];
+        			diagonalSum2+=arr[y][n-1-x];
+        		}
+        	}	
+        }
+        
+        for(int i=0; i<n; i++) {
+        	answer=Math.max(rowSum[i], answer); 
+        	answer=Math.max(colSum[i], answer);
+        }
+        
+        answer=Math.max(diagonalSum1, answer);
+        answer=Math.max(diagonalSum2, answer);
 
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[][] arr = new int[n][n];
-		for( int y=0; y<n; y++ ) {
-			for( int x=0; x<n; x++ ) {
-				arr[y][x]=sc.nextInt();
-			}
-		}
-		System.out.println(T.solution(n, arr));
-	}
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                arr[y][x] = sc.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
+    }
 }
