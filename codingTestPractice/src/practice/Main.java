4d9b77ea0b36a01ff7fe237b2013fd81551ae218
da0293 <dy7133@naver.com>
2023-12-19 01:34:44 +0900

    commit 11. 임시반장 정하기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,41 +2,38 @@ package practice;
 
 import java.util.Scanner;
 
-public class Main {
-	int[] dx= { -1, 0, 1, 0 };
-	int[] dy= { 0, 1, 0, -1 };
-    public int solution(int n, int[][] arr) {
-        int answer = 0;
-        for(int y=0; y<n; y++) {
-        	for(int x=0; x<n; x++) {
-        		boolean flag=true; 
-        		for( int k=0; k<4; k++) {
-        			// 해당 좌표의 상하좌우 좌표값
-        			int nx=dx[k]+x; 
-        			int ny=dy[k]+y;
-        			// 봉우리가 아닌 것 먼저 구하기
-        			// ArrayIndexOutOfBoundaryError를 방지 하기위해 nx>=0 && nx<n && ny>=0 && ny<n 먼저 설젇
-        			if( nx>=0 && nx<n && ny>=0 && ny<n && arr[ny][nx]>=arr[y][x]) {
-        				flag=false;
-        				break; 
-        			}
-        		}
-        		if(flag) answer++; 
-        	}
-        }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                arr[y][x] = sc.nextInt();
-            }
-        }
-        System.out.println(T.solution(n, arr));
-    }
+public class Main {	
+	public int solution(int n, int[][] arr){
+		int answer=0, max=Integer.MIN_VALUE;
+		for( int i=1; i<=n; i++) {  // 사람 수 
+			int cnt=0; 
+			for(int j=1; j<=n; j++) {   // 사람 수 
+				for(int k=1; k<=5; k++) { // 학년 수 
+					if(arr[i][k]==arr[j][k]) { 
+						cnt++;
+						break; // 한사람이 2,3학년때 같은반일경우 2로 cnt가 되는것 막기위해
+					}
+				}
+			}
+			if(cnt>max) {
+				max=cnt;
+				answer=i;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n+1][6]; // [사람 수 +1][학년+1] //1~5를 맞춰서 쓰려고 
+		for( int i=1; i<n+1; i++) { //  <사람 입력 수 +1
+			for( int j = 1; j <=5; j++) { // <= 학년 수 
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print(T.solution(n, arr));
+	}
 }
