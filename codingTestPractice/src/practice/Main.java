af60ae26857b0fc72978e6a197e60a381ea5decb
da0293 <dy7133@naver.com>
2023-12-19 15:15:24 +0900

    commit 12. 멘토링

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,38 +2,48 @@ package practice;
 
 import java.util.Scanner;
 
-public class Main {	
-	public int solution(int n, int[][] arr){
-		int answer=0, max=Integer.MIN_VALUE;
-		for( int i=1; i<=n; i++) {  // 사람 수 
-			int cnt=0; 
-			for(int j=1; j<=n; j++) {   // 사람 수 
-				for(int k=1; k<=5; k++) { // 학년 수 
-					if(arr[i][k]==arr[j][k]) { 
-						cnt++;
-						break; // 한사람이 2,3학년때 같은반일경우 2로 cnt가 되는것 막기위해
-					}
-				}
-			}
-			if(cnt>max) {
-				max=cnt;
-				answer=i;
-			}
-		}
-		
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[][] arr = new int[n+1][6]; // [사람 수 +1][학년+1] //1~5를 맞춰서 쓰려고 
-		for( int i=1; i<n+1; i++) { //  <사람 입력 수 +1
-			for( int j = 1; j <=5; j++) { // <= 학년 수 
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		System.out.print(T.solution(n, arr));
-	}
+public class Main {
+    public int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                boolean isMentor = true;
+
+                for (int k = 0; k < m; k++) {
+                    int mentorRank = 0;
+                    int menteeRank = 0;
+
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) mentorRank = s;
+                        if (arr[k][s] == j) menteeRank = s;
+                    }
+
+                    if (mentorRank >= menteeRank) {
+                        isMentor = false;
+                        break;
+                    }
+                }
+
+                if (isMentor) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
 }
