77291302962866f86717cc95a92f9d15e9b90176
da0293 <dy7133@naver.com>
2023-12-21 21:30:14 +0900

    commit 6. 뒤집은 소수

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,26 +1,43 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public int solution(int n){
-		int answer = 0;
-		int[] ck =  new int[n+1]; 
-		for( int i=2; i<n+1; i++ ) {
-			if(ck[i]==0) {
-				answer++; 
-				for( int j=i; j<n+1; j+=i) {
-					ck[j]=1; 
-				}
+	public ArrayList<Integer> solution(int n, int []arr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		boolean isPrime=true; 
+		for(int i=0; i<n; i++) {
+			int a=arr[i]; 
+			int temp=0; 
+			while(a>0) {
+				int remainder=a%10; 
+				temp = (temp)*10+remainder;
+				a=a/10; 
 			}
+			if(isPrime(temp)) answer.add(temp); 
 		}
 		return answer;
 	}
 	
+	private boolean isPrime(int temp) {
+		if( temp==1 )return false; 
+		for( int i=2; i<temp; i++ ) {
+			if(temp%i==0)return false; 
+		}
+		return true; 
+	}
+
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.print(T.solution(n));
+		int[] arr = new int[n]; 
+		for( int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for( int x : T.solution(n, arr) ) {
+			System.out.print(x + " ");
+		}
 	}
 }
