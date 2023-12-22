f8dddee09e90dce57d3e7e61287757c09a861556
da0293 <dy7133@naver.com>
2023-12-22 17:34:37 +0900

    commit 9. 격자판 최대합

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,21 +1,33 @@
 package practice;
 
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public ArrayList<Integer> solution(int n, int []arr){
-		ArrayList<Integer> answer = new ArrayList<>();		
-		for( int i=0; i<arr.length; i++ ) {
-			int rank=1;  
-			for( int j=0; j<arr.length; j++ ) {
-				if(arr[i]<arr[j]) {
-					rank++;
-				}
-			}
-			answer.add(rank); 
-		}
+	public int solution(int n, int [][]arr){
+		int answer = 0; 
+		int colSum[] = new int[n]; 
+		int rowSum[] = new int[n]; 
+ 		int leftDiagonal=0, rightDiagonal=0; 
+ 		for(int y=0; y<n; y++) {
+ 			for( int x=0; x<n; x++) {
+ 				colSum[y]+=arr[y][x]; 
+ 				rowSum[y]+=arr[x][y]; 
+ 				if(y==x) {
+ 	 				leftDiagonal+=arr[y][x]; 
+ 	 			}
+ 				if((y+x)==(n-1)) {
+ 					rightDiagonal+=arr[y][x]; 
+ 				}
+ 			}
+ 		}
+ 		
+ 		for( int i=0; i<n; i++) {
+ 			answer=Math.max(answer, colSum[i]); 
+ 			answer=Math.max(answer, rowSum[i]);
+ 		}
+ 		answer= Math.max(leftDiagonal, answer);
+ 		answer= Math.max(rightDiagonal, answer);
 		return answer;
 	}
 	
@@ -23,13 +35,13 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int arr[] = new int[n];
+		int arr[][] = new int[n][n];
 		for( int i=0; i< n; i++) {
-			arr[i] = sc.nextInt();
+			for( int j = 0; j<n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
 		}
-		for( int x : T.solution(n, arr)) {
-			System.out.print(x +" ");
-		}
+		System.out.print(T.solution(n, arr));
 	}
 }
 
