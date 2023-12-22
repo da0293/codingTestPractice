1923857a33ef4b38c9debc1a395d1f1fe708ec45
da0293 <dy7133@naver.com>
2023-12-22 17:02:52 +0900

    commit 7. 점수계산

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,31 +1,18 @@
 package practice;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public ArrayList<Integer> solution(int n, int []arr){
-		ArrayList<Integer> answer = new ArrayList<>();
-		boolean isPrime=true; 
-		for(int i=0; i<n; i++) {
-			int a=arr[i]; 
-			int temp=0; 
-			while(a>0) {
-				int remainder=a%10; 
-				temp = (temp)*10+remainder;
-				a=a/10; 
+	public int solution(int n, int []arr){
+		int answer = 0, temp=0; 
+		for( int i=0; i<arr.length; i++) {
+			if(arr[i]==1) temp++; 
+			else {
+				temp=0; 
 			}
-			if(isPrime(temp)) answer.add(temp); 
+			answer+=temp;
 		}
 		return answer;
-	}
-	
-	private boolean isPrime(int temp) {
-		if( temp==1 )return false; 
-		for( int i=2; i<temp; i++ ) {
-			if(temp%i==0)return false; 
-		}
-		return true; 
 	}
 
 	public static void main(String[] args) {
@@ -36,8 +23,6 @@ public class Main {
 		for( int i=0; i<n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		for( int x : T.solution(n, arr) ) {
-			System.out.print(x + " ");
-		}
+		System.out.println(T.solution(n, arr));
 	}
 }
