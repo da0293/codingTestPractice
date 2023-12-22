a183c2cdf16879c0cada632412a0096083c2b059
da0293 <dy7133@naver.com>
2023-12-22 19:57:12 +0900

    commit 10. 봉우리

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -4,30 +4,24 @@ package practice;
 import java.util.Scanner;
 
 public class Main {
-	public int solution(int n, int [][]arr){
-		int answer = 0; 
-		int colSum[] = new int[n]; 
-		int rowSum[] = new int[n]; 
- 		int leftDiagonal=0, rightDiagonal=0; 
- 		for(int y=0; y<n; y++) {
- 			for( int x=0; x<n; x++) {
- 				colSum[y]+=arr[y][x]; 
- 				rowSum[y]+=arr[x][y]; 
- 				if(y==x) {
- 	 				leftDiagonal+=arr[y][x]; 
- 	 			}
- 				if((y+x)==(n-1)) {
- 					rightDiagonal+=arr[y][x]; 
- 				}
- 			}
- 		}
- 		
- 		for( int i=0; i<n; i++) {
- 			answer=Math.max(answer, colSum[i]); 
- 			answer=Math.max(answer, rowSum[i]);
- 		}
- 		answer= Math.max(leftDiagonal, answer);
- 		answer= Math.max(rightDiagonal, answer);
+	int[] dx = { -1, 0, 1, 0 };
+	int[] dy = { 0, -1, 0, 1 }; 
+	public int solution(int n, int[][] arr){
+		int answer = 0;
+		for( int i=0; i<n; i++ ) {
+			for( int j=0; j<n; j++ ) {
+				boolean flag=true; 
+				for( int k=0; k<4; k++) {
+					int ny=i+dy[k]; 
+					int nx=j+dx[k];
+					if( ny >= 0 && ny < n && nx >= 0 && nx < n && arr[ny][nx] >= arr[i][j] ) {
+						flag=false; 
+						break; 
+					}
+				}
+				if(flag) answer++; 
+			}
+		}
 		return answer;
 	}
 	
@@ -35,7 +29,7 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int arr[][] = new int[n][n];
+		int[][] arr = new int[n][n];
 		for( int i=0; i< n; i++) {
 			for( int j = 0; j<n; j++) {
 				arr[i][j] = sc.nextInt();
@@ -44,4 +38,5 @@ public class Main {
 		System.out.print(T.solution(n, arr));
 	}
 }
+
 
