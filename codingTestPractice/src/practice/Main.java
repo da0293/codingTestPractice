633af7423bdfb960c14be33eda6313ee04c8232b
da0293 <dy7133@naver.com>
2023-12-27 23:53:26 +0900

    commit 2. 공통원소 구하기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,28 +1,20 @@
 package practice;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {	
-	public int solution(int n, int m, int[][] arr){
-		int answer=0;
-		for( int i=1; i<=n; i++ ) {
-			for( int j=1; j<=n; j++ ) {
-				if( i==j )continue; 
-				boolean isMentor=true;
-				for( int t=0; t<m; t++ ) {
-					int mentoRank=0; 
-					int menteeRank=0; 
-					for( int s=0; s<n; s++) {
-						if(arr[t][s]==i) mentoRank=s; 
-						if(arr[t][s]==j) menteeRank=s; 
-					}
-					if(mentoRank >= menteeRank) {
-						isMentor=false; 
-						break; 
-					}
+	public ArrayList<Integer> solution(int n, int m, int[] arr, int[] brr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		Arrays.sort(arr); 
+		Arrays.sort(brr);
+		for( int i=0; i<n; i++) {
+			for( int j=0; j<m; j++) {
+				if( arr[i]==brr[j] ) {
+					answer.add(arr[i]);
 				}
-				if(isMentor) answer++; 
 			}
 		}
 		return answer;
@@ -32,14 +24,18 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n =  sc.nextInt();
-		int m = sc.nextInt();
-		int[][] arr = new int[m][n]; 
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				arr[i][j] = sc.nextInt();
-			}
+		int[] arr = new int[n]; 
+		for( int i=0; i<n; i++ ) {
+			arr[i] = sc.nextInt();
 		}
-		System.out.print(T.solution(n,m, arr));
+		int m = sc.nextInt();
+		int[] brr = new int[m]; 
+		for( int i=0; i<m; i++) {
+			brr[i] = sc.nextInt();
+		}
+		for(int x: T.solution(n, m, arr, brr)) {
+			System.out.print(x + " ");
+		}
 	}
 }
 
