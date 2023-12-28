cece0eb2f75c2367d4521c2a3bdc784d9a1054fd
da0293 <dy7133@naver.com>
2023-12-28 21:28:16 +0900

    commit 3. 최대 매출

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,25 +1,16 @@
 package practice;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {	
-	public ArrayList<Integer> solution(int n, int m, int[] arr, int[] brr){
-		ArrayList<Integer> answer = new ArrayList<>();
-		Arrays.sort(arr); 
-		Arrays.sort(brr);
-		int p1=0, p2=0; 
-		while(p1<n && p2<m) {
-			if(arr[p1]==brr[p2]) {
-				answer.add(arr[p1++]);
-				p2++; 
-			}else if(arr[p1]<brr[p2]) {
-				p1++; 
-			}else {
-				p2++; 
-			}
+	public int solution(int n, int k, int[] arr){
+		int answer = 0, sum=0; 
+		for(int i=0; i<k; i++) sum+=arr[i]; 
+		answer=sum; // 초기 윈도우 설정
+		for(int i=k; i<n; i++) { 
+			sum+=(arr[i]-arr[i-k]); // 윈도우 맨 앞 수는 제외, 배열의 새로운 수는 합침
+			answer=Math.max(answer,sum); // 최대값 설정
 		}
 		return answer;
 	}
@@ -27,19 +18,13 @@ public class Main {
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n =  sc.nextInt();
+		int n = sc.nextInt();
+		int k = sc.nextInt();
 		int[] arr = new int[n]; 
 		for( int i=0; i<n; i++ ) {
 			arr[i] = sc.nextInt();
 		}
-		int m = sc.nextInt();
-		int[] brr = new int[m]; 
-		for( int i=0; i<m; i++) {
-			brr[i] = sc.nextInt();
-		}
-		for(int x: T.solution(n, m, arr, brr)) {
-			System.out.print(x + " ");
-		}
+		System.out.println(T.solution(n, k, arr));
 	}
 }
 
