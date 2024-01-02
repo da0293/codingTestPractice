eca9364c073c06b0f9d2fefd721a7416be20c5fb
da0293 <dy7133@naver.com>
2024-01-02 23:57:58 +0900

    연속된 자연수의 합

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -5,15 +5,17 @@ package practice;
 import java.util.Scanner;
 
 public class Main {	
-	public int solution(int n, int k, int[] arr){
-		int answer=0, sum=0, lt=0; ; 
-		for( int i=0; i<k; i++) {
-			sum+=arr[i]; 
-		}
-		answer = Math.max(answer, sum);
-		for( int rt=k; rt<n; rt++) {
-			sum+=(arr[rt]-arr[lt++]); 
-			answer = Math.max(answer, sum);
+	public int solution(int n){
+		int answer=0, sum=0, lt=0; 
+		int m = n/2+1;
+		int[] a= new int[m];
+		for(int i=0; i<m; i++) a[i]=i+1; // 자연수니까 i+1함 
+		for( int rt=0; rt<m; rt++ ) {
+			sum+=a[rt]; 
+			while(sum>n) {
+				sum-=a[lt++];
+			}
+			if(sum==n)answer++;
 		}
 		return answer;
 	}
@@ -21,12 +23,7 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int k = sc.nextInt();
-		int[] arr = new int[n]; 
-		for( int i=0; i<n; i++ ) {
-			arr[i] = sc.nextInt();
-		}
-		System.out.println(T.solution(n, k, arr));
+		System.out.println(T.solution(n));
 	}
 }
 
