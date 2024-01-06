34f09a14b8404c1aecfceba2600f8264966070bf
da0293 <dy7133@naver.com>
2024-01-06 23:41:16 +0900

    commit 5. K번째 큰 수

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,47 +2,43 @@ package practice;
 
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {	
-	public int solution(String s, String t){
-		int answer=0; 
-		int sLen = s.length();
-		int tLen = t.length();
-		HashMap<Character, Integer> targetMap = new HashMap<>();
-		HashMap<Character, Integer> windowMap= new HashMap<>();
-		
-		// 타겟 해시맵
-		for( int i=0; i<tLen; i++ ){
-			if(targetMap.containsKey(t.charAt(i))) targetMap.put(t.charAt(i), targetMap.get(t.charAt(i)+1));
-			else targetMap.put(t.charAt(i), 1);
+	public int solution(int n, int k, int[] a) {
+		int answer =-1;
+		TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
+		// 아무것도 없으면 오름차순,  Collections.reverseOrder() 내림차순 
+		for( int i=0; i<n-2; i++) {
+			for(int j=i+1; j<n-1; j++) {
+				for(int l=j+1; l<n; l++) {
+					ts.add(a[i]+a[j]+a[l]); // .add는 set자료구조에서 추가할 때 사용
+				}
+			}
 		}
-		
-		// 초기 윈도우 창
-		for( int i=0; i<tLen; i++) {
-			if(windowMap.containsKey(s.charAt(i))) windowMap.put(s.charAt(i), windowMap.get(s.charAt(i)+1));
-			else windowMap.put(s.charAt(i), 1);
+		int cnt=0;
+		for(int x: ts) { // Treeset하나씩 빼올때 
+			cnt++; //cnt가 마치 내림차순한 배열 인덱스 같다. 
+			if(cnt==k)return x; 		
 		}
-		
-		// 타겟 맵과 초기윈도우가 같으면 answer++;
-		if(targetMap.equals(windowMap)) answer++; 
-		
-		// 윈도우맵과 타겟맵 비교
-		int lt=0; 
-		for( int rt=tLen; rt<sLen; rt++ ) {
-
-			
-		}
-		
 		return answer; 
+		
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		String t = sc.next();
-		System.out.println(T.solution(s,t));
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0; i<n; i++) {
+			a[i]=sc.nextInt();
+		}
+		System.out.print(T.solution(n, k, a));
 	}
 }
 
