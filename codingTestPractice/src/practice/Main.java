c38917dd7dfa30c5fd5242ae9c1008a075a93da8
da0293 <dy7133@naver.com>
2024-01-09 22:54:29 +0900

    commit 5. K번째 큰 수

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,29 +2,25 @@ package practice;
 
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {	
-	public int solution(int n, int k, int[] a) {
-		int answer =-1;
+	public int solution(int n, int k, int[] arr) {
+		int answer=-1; 
 		TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
-		// 아무것도 없으면 오름차순,  Collections.reverseOrder() 내림차순 
-		for( int i=0; i<n-2; i++) {
-			for(int j=i+1; j<n-1; j++) {
-				for(int l=j+1; l<n; l++) {
-					ts.add(a[i]+a[j]+a[l]); // .add는 set자료구조에서 추가할 때 사용
+		for( int i=0 ; i<n; i++) {
+			for( int j=i+1; j<n; j++ ) {
+				for( int t=j+1; t<n; t++) {
+					ts.add(arr[i]+arr[j]+arr[t]);
 				}
 			}
 		}
-		int cnt=0;
-		for(int x: ts) { // Treeset하나씩 빼올때 
-			cnt++; //cnt가 마치 내림차순한 배열 인덱스 같다. 
-			if(cnt==k)return x; 		
+		int cnt=0; 
+		for( int x : ts ) {
+			cnt++; 
+			if( cnt == k ) answer=x;
 		}
 		return answer; 
 		
@@ -34,11 +30,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n=sc.nextInt();
 		int k=sc.nextInt();
-		int[] a=new int[n];
+		int[] arr=new int[n];
 		for(int i=0; i<n; i++) {
-			a[i]=sc.nextInt();
+			arr[i]=sc.nextInt();
 		}
-		System.out.print(T.solution(n, k, a));
+		System.out.print(T.solution(n, k, arr));
 	}
 }
 
