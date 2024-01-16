82ffd89a8dfb27a72b64a547593fbc4083e6cc38
da0293 <dy7133@naver.com>
2024-01-16 14:04:11 +0900

    commit 괄호문자제거

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,39 +2,30 @@ package practice;
 
 
 
-import java.util.Collections;
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.Stack;
 
 public class Main {	
-	public int solution(int n, int k, int[] arr) {
-		int answer=-1; 
-		TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
-		for( int i=0 ; i<n; i++) {
-			for( int j=i+1; j<n; j++ ) {
-				for( int t=j+1; t<n; t++) {
-					ts.add(arr[i]+arr[j]+arr[t]);
-				}
+	public String solution(String str) {
+		String answer = ""; 
+		Stack<Character> stack = new Stack<>();
+		for( char x :  str.toCharArray()) {
+			if(x==')') {
+				while(stack.pop()!='(');
+			}else {
+				stack.push(x);
 			}
 		}
-		int cnt=0; 
-		for( int x : ts ) {
-			cnt++; 
-			if( cnt == k ) answer=x;
+		for( int i=0; i<stack.size(); i++ ) {
+			answer+=stack.get(i); 
 		}
 		return answer; 
-		
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
-		int k=sc.nextInt();
-		int[] arr=new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i]=sc.nextInt();
-		}
-		System.out.print(T.solution(n, k, arr));
+		String str=sc.next();
+		System.out.print(T.solution(str));
 	}
 }
 
