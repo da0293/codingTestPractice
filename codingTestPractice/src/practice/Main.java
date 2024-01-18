b57206263ef93ab051217b6441ae3b45361c6c6d
da0293 <dy7133@naver.com>
2024-01-18 19:35:05 +0900

    commit 크레인 인형뽑기(카카오)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -6,26 +6,43 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {	
-	public String solution(String str) {
-		String answer = ""; 
-		Stack<Character> stack = new Stack<>();
-		for( char x :  str.toCharArray()) {
-			if(x==')') {
-				while(stack.pop()!='(');
-			}else {
-				stack.push(x);
+	public int solution(int n, int[][] board, int m, int[] moves) {
+		int answer = 0; 
+		Stack<Integer> stack = new Stack<>();
+		for( int movesX : moves ) {
+			for( int y=0; y<n; y++ ) {
+				int x = movesX-1;
+				if(board[y][x]!=0) {
+					int movesIndex=board[y][x];
+					board[y][x]=0; 
+					if(!stack.isEmpty()&& movesIndex==stack.peek()) {
+						answer+=2; 
+						stack.pop();
+					}else {
+						stack.push(movesIndex);
+					}
+					break; 
+				}
 			}
-		}
-		for( int i=0; i<stack.size(); i++ ) {
-			answer+=stack.get(i); 
 		}
 		return answer; 
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String str=sc.next();
-		System.out.print(T.solution(str));
+		int n = sc.nextInt();
+		int[][] board = new int[n][n];
+		for( int y=0; y<n; y++ ) {
+			for( int x=0; x<n; x++) {
+				board[y][x] = sc.nextInt();
+			}
+		}
+		int m = sc.nextInt();
+		int[] moves = new int[m];
+		for( int i=0; i<m; i++) {
+			moves[i] = sc.nextInt();
+		}
+		System.out.print(T.solution(n, board, m, moves));
 	}
 }
 
