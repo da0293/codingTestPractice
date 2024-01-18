0f3effd0368a00c397d3429016313cf603a996fb
da0293 <dy7133@naver.com>
2024-01-18 21:20:51 +0900

    commit 후위식 연산

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -6,43 +6,39 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {	
-	public int solution(int n, int[][] board, int m, int[] moves) {
+	public int solution(String str) {
 		int answer = 0; 
 		Stack<Integer> stack = new Stack<>();
-		for( int movesX : moves ) {
-			for( int y=0; y<n; y++ ) {
-				int x = movesX-1;
-				if(board[y][x]!=0) {
-					int movesIndex=board[y][x];
-					board[y][x]=0; 
-					if(!stack.isEmpty()&& movesIndex==stack.peek()) {
-						answer+=2; 
-						stack.pop();
-					}else {
-						stack.push(movesIndex);
-					}
-					break; 
+		for( char x : str.toCharArray()) {
+			if(Character.isDigit(x)) {
+				stack.push(Character.getNumericValue(x)); 
+			}else {
+				int b = stack.pop();
+				int a = stack.pop();
+				switch(x) {
+				case '+':
+					stack.push(a+b); 
+					break;
+				case '-':
+					stack.push(a-b);
+					break;
+				case '*':
+					stack.push(a*b);
+					break;
+				case '/':
+					stack.push(a/b);
+					break;
 				}
 			}
 		}
+		answer = stack.pop();
 		return answer; 
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[][] board = new int[n][n];
-		for( int y=0; y<n; y++ ) {
-			for( int x=0; x<n; x++) {
-				board[y][x] = sc.nextInt();
-			}
-		}
-		int m = sc.nextInt();
-		int[] moves = new int[m];
-		for( int i=0; i<m; i++) {
-			moves[i] = sc.nextInt();
-		}
-		System.out.print(T.solution(n, board, m, moves));
+		String s = sc.next();
+		System.out.print(T.solution(s));
 	}
 }
 
