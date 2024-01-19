9521f4611b64f879b24d9b74cee61dde6fedcf0b
da0293 <dy7133@naver.com>
2024-01-19 22:13:48 +0900

    commit 쇠막대기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -9,29 +9,23 @@ public class Main {
 	public int solution(String str) {
 		int answer = 0; 
 		Stack<Integer> stack = new Stack<>();
-		for( char x : str.toCharArray()) {
-			if(Character.isDigit(x)) {
-				stack.push(Character.getNumericValue(x)); 
-			}else {
-				int b = stack.pop();
-				int a = stack.pop();
-				switch(x) {
-				case '+':
-					stack.push(a+b); 
-					break;
-				case '-':
-					stack.push(a-b);
-					break;
-				case '*':
-					stack.push(a*b);
-					break;
-				case '/':
-					stack.push(a/b);
-					break;
-				}
-			}
-		}
-		answer = stack.pop();
+		for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                stack.push(i); // 여는 괄호의 인덱스를 스택에 저장
+            } else {
+                if (stack.peek() + 1 == i) {
+                    // 레이저인 경우
+                    stack.pop();
+                    answer += stack.size();
+                } else {
+                    // 쇠막대기 끝인 경우
+                    stack.pop();
+                    answer++;
+                }
+            }
+        }
+
 		return answer; 
 	}
 	public static void main(String[] args) {
@@ -41,5 +35,3 @@ public class Main {
 		System.out.print(T.solution(s));
 	}
 }
-
-
