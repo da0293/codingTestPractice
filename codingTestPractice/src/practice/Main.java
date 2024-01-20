8498bfe62e8590bd04e2a6c2f64f56f0fc59fec4
da0293 <dy7133@naver.com>
2024-01-20 22:49:06 +0900

    commit 6. 공주 구하기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,37 +1,34 @@
 package practice;
 
-
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {	
-	public int solution(String str) {
+	public int solution(int n, int k) {
 		int answer = 0; 
-		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == '(') {
-                stack.push(i); // 여는 괄호의 인덱스를 스택에 저장
-            } else {
-                if (stack.peek() + 1 == i) {
-                    // 레이저인 경우
-                    stack.pop();
-                    answer += stack.size();
-                } else {
-                    // 쇠막대기 끝인 경우
-                    stack.pop();
-                    answer++;
-                }
-            }
-        }
-
+		Queue<Integer> q = new LinkedList<>();
+		for( int i=1; i<=n; i++) {
+			q.offer(i); 
+		}
+		int cnt=0; 
+		while(q.size()>1) {
+			cnt++;
+			if( cnt==k ){
+				q.poll();
+				cnt=0; 
+			} else {
+				q.offer(q.poll());
+			}
+		}
+		answer = q.poll(); 
 		return answer; 
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		System.out.print(T.solution(s));
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.print(T.solution(n, k));
 	}
 }
