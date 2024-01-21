bb980c14532453411273fda49fbfedba64eaf1f8
da0293 <dy7133@naver.com>
2024-01-22 01:21:41 +0900

    commit 7. 교육과정 설계

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,34 +1,31 @@
 package practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {	
-	public int solution(int n, int k) {
-		int answer = 0; 
-		Queue<Integer> q = new LinkedList<>();
-		for( int i=1; i<=n; i++) {
-			q.offer(i); 
-		}
-		int cnt=0; 
-		while(q.size()>1) {
-			cnt++;
-			if( cnt==k ){
-				q.poll();
-				cnt=0; 
-			} else {
-				q.offer(q.poll());
+	public String solution(String seq, String plan) {
+		String answer = "NO"; 
+		char[] s = seq.toCharArray();
+		char[] p = plan.toCharArray();
+		int sLen = s.length;
+		int pLen = p.length;
+		int sIndex=0; 
+		for( int i=0; i<pLen; i++ ) {
+			if(s[sIndex]==p[i]) {
+				sIndex++; 
+				if(sIndex==sLen) {
+					answer = "YES";
+					break; 
+				}
 			}
 		}
-		answer = q.poll(); 
 		return answer; 
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		System.out.print(T.solution(n, k));
+		String seq = sc.next();
+		String plan = sc.next();
+		System.out.print(T.solution(seq, plan));
 	}
 }
