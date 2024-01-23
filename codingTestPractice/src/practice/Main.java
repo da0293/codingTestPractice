6375f127986d68d34129067d493cfa56813d0b15
da0293 <dy7133@naver.com>
2024-01-23 11:07:38 +0900

    commit 수업계획짜기

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,24 +1,20 @@
 package practice;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {	
 	public String solution(String seq, String plan) {
-		String answer = "NO"; 
-		char[] s = seq.toCharArray();
-		char[] p = plan.toCharArray();
-		int sLen = s.length;
-		int pLen = p.length;
-		int sIndex=0; 
-		for( int i=0; i<pLen; i++ ) {
-			if(s[sIndex]==p[i]) {
-				sIndex++; 
-				if(sIndex==sLen) {
-					answer = "YES";
-					break; 
-				}
+		String answer = "YES"; 
+		Queue<Character> Q = new LinkedList<>();
+		for(char x : seq.toCharArray())Q.offer(x);
+		for(char x : plan.toCharArray()) {
+			if(Q.contains(x)) {
+				if(x!=Q.poll())return "NO";
 			}
 		}
+		if(!Q.isEmpty()) return "NO"; 
 		return answer; 
 	}
 	public static void main(String[] args) {
