9e041af04bd9e6b263bb50d36eb237fa17ddd3b3
da0293 <dy7133@naver.com>
2024-01-25 10:35:23 +0900

    commit. 7. 교육과정 설계

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -5,37 +5,19 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {	
-	static class Patient {
-		int id; 
-	    int priority; 
-	    
-	    public Patient(int id, int priority) {
-	        this.id = id; 
-	        this.priority = priority; 
-	    }
-	}
-	public int solution(int n, int m, int[] waitingList) {
-		int answer = 0; 
-		Queue<Patient> q = new LinkedList<>();
-		// 환자들을 큐에 추가 
-		for(int i=0; i<n; i++) {
-			q.offer(new Patient(i, waitingList[i])); 
-		}
-		// 큐가 비어있기 직전까지 while문
-		while(!q.isEmpty()) {
-			// 63
-			Patient current = q.poll(); 
-			boolean canTreat = true; 
-			for(Patient compare : q) {
-				if(current.priority < compare.priority) {
-					q.add(current);
-					canTreat = false;
+	public String solution(String seq, String plan) {
+		String answer = "NO"; 
+		Queue<Integer> Q = new LinkedList<>();
+		char[] s = seq.toCharArray();
+		char[] p = plan.toCharArray();
+		int sIndex=0; 
+		for( int i=0; i<p.length; i++ ) {
+			if(s[sIndex]==p[i]) {
+				sIndex++; 
+				if(sIndex==s.length) {
+					answer="YES"; 
 					break; 
 				}
-			}
-			if(canTreat==true) {
-				answer++; 
-				if(m==current.id)return answer; 
 			}
 		}
 		return answer; 
@@ -43,12 +25,8 @@ public class Main {
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] waitingList = new int[n]; 
-		for( int i=0; i<n; i++ ) {
-			waitingList[i] = sc.nextInt();
-		}
-		System.out.print(T.solution(n, m, waitingList));
+		String seq = sc.next();
+		String plan = sc.next();
+		System.out.print(T.solution(seq, plan));
 	}
 }
