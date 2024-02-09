3e22101cc4611883ef249bdc871f3831c639a920
da0293 <dy7133@naver.com>
2024-02-09 22:51:29 +0900

    commit 삽입정렬

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Main {	
 	public int[] solution(int n, int[] arr) {
-		for( int i=0; i<n-1; i++ ) {
-			for( int j=0; j<n-1-i; j++ ) {
-				if(arr[j]>arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
+		for( int i=1; i<n; i++ ) {
+			int temp=arr[i];
+			int j;
+			for( j=i-1; j>=0; j-- ) {
+				if(temp<arr[j]) {
+					arr[j+1]=arr[j]; 
+				} else break; 
 			}
+			arr[j+1]=temp; 
 		}
 		return arr;
 	}
