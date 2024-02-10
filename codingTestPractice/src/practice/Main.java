12c820c7385588d2b09dae3c5a7e38331dc82624
da0293 <dy7133@naver.com>
2024-02-10 14:19:33 +0900

    commit Least Recently Used

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -3,28 +3,36 @@ package practice;
 import java.util.Scanner;
 
 public class Main {	
-	public int[] solution(int n, int[] arr) {
-		for( int i=1; i<n; i++ ) {
-			int temp=arr[i];
-			int j;
-			for( j=i-1; j>=0; j-- ) {
-				if(temp<arr[j]) {
-					arr[j+1]=arr[j]; 
-				} else break; 
+	public int[] solution(int size, int n, int[] arr) {
+		int[] cache = new int[size]; 
+		for( int x : arr ) {
+			int targetIdx = -1; 
+			for( int i=0; i<size; i++ ) {
+				if(cache[i]==x) targetIdx=i; 
 			}
-			arr[j+1]=temp; 
+			if(targetIdx!=-1) {
+				for(int i=targetIdx; i>=1; i--) {
+					cache[i]=cache[i-1]; 
+				}
+			} else {
+				for( int i=size-1; i>=1; i-- ) {
+					cache[i]=cache[i-1]; 
+				}
+			}
+			cache[0]=x;
 		}
-		return arr;
+		return cache;
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
 		int n = sc.nextInt();
 		int[] arr = new int[n]; 
 		for( int i=0; i<n; i++ ) {
 			arr[i] = sc.nextInt();
 		}
-		for( int x : T.solution(n, arr)) {
+		for( int x : T.solution(s, n, arr)) {
 			System.out.print(x + " ");
 		}
 	}
