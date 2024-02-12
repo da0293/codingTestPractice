62d0c7e786749d2d8fe40269ca1a5dc673e787c8
da0293 <dy7133@naver.com>
2024-02-12 23:41:12 +0900

    commit 7. 좌표 정렬

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,32 +1,35 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {	
-	public ArrayList<Integer> solution(int n, int[] arr) {
-		ArrayList<Integer> answer = new ArrayList<>();
-		int[] brr=new int[n];
-		brr=arr.clone();
-		Arrays.sort(arr);
-		for( int i=0; i<n; i++) {
-			if(arr[i]!=brr[i]) {
-				answer.add(i+1);
-			}
-		}
-		return answer;
+class Point implements Comparable<Point> {
+	int x;
+	int y; 
+	public Point(int x, int y) {
+		this.x=x; 
+		this.y=y;
 	}
+	@Override
+	public int compareTo(Point o) {
+		if(this.x==o.x) return o.y-this.y;
+		else return o.x-this.x;
+	}
+}
+public class Main {	
 	public static void main(String[] args) {
-		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n]; 
+		ArrayList<Point> arr = new ArrayList<>();
 		for( int i=0; i<n; i++ ) {
-			arr[i] = sc.nextInt();
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			arr.add(new Point(x,y));
 		}
-		for(int x: T.solution(n, arr)) {
-			System.out.print(x + " " );
+		Collections.sort(arr,Collections.reverseOrder());
+		for(Point o :  arr) {
+			System.out.println(o.x+ " " +o.y);
 		}
 	}
 }
