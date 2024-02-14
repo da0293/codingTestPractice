81b4fdb1aaf259e43268c39a353c0b143b3efe20
da0293 <dy7133@naver.com>
2024-02-15 03:27:29 +0900

    commit 7. 좌표 정렬

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,44 +1,37 @@
 package practice;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
+class Point implements Comparable<Point>{
+	int x; 
+	int y;
+	public Point(int x, int y) {
+		this.x=x; 
+		this.y=y; 
+	}
+	@Override
+	public int compareTo(Point o) {
+		if(this.x==o.x)return this.y-o.y; 
+		else return this.x-o.x; 
+	}
+	
+}
 
 public class Main {	
-	public int count(int[] arr, int capacity) {
-		int cnt=1, sum=0; 
-		for(int x: arr) {
-			if(sum+x>capacity) {
-				cnt++;
-				sum=x; 
-			}else {
-				sum+=x; 
-			}
-		}
-		return cnt; 
-	}
-	public int solution(int n, int m, int[] arr) {
-		int answer=0; 
-		int lt=Arrays.stream(arr).max().getAsInt();
-		int rt=Arrays.stream(arr).sum();
-		while(lt<rt) {
-			int mid=(lt+rt)/2; 
-			if(count(arr,mid)<=m) {
-				answer=mid;
-				rt=mid-1;
-			}else {
-				lt=mid+1;
-			}
-		}
-		return answer;
-	}
 	public static void main(String[] args) {
-		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] arr = new int[n]; 
-		for(int i=0; i<n; i++) arr[i]=sc.nextInt();
-		System.out.println(T.solution(n, m, arr));
+		ArrayList<Point> list = new ArrayList<>();
+		for( int i=0; i<n; i++ ) {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			list.add(new Point(x,y));
+		}
+		Collections.sort(list);
+		for( Point p : list ) {
+			System.out.println(p.x + " " + p.y);
+		}
 	}
 }
