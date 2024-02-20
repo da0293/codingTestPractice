2fc7550eb404ece8d5b551314ef4a665758ab442
da0293 <dy7133@naver.com>
2024-02-20 19:42:56 +0900

    commit 피보나치 수열 재귀

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -3,19 +3,24 @@ package practice;
 import java.util.Scanner;
 
 public class Main {	
-	/*
-	 * D(5) 5*DFS(4)를 리턴받음 -> D(4) 4*DFS(3)을 리턴받음
-	 * -> D(3) 3*DFS(2)를 리턴받음 -> D(2) 2*DFS(1)을 리턴받음 -> 1 리턴받고 pop
-	 */
+	static int[] fibo; 
 	public int DFS(int n) {
+		// 이미구한 값
+		if(fibo[n]>0) return fibo[n]; 
 		if(n==1) return 1; 
-		else return n*DFS(n-1); 
+		else if(n==2) return 1; 
+		else {
+			return fibo[n]=DFS(n-2)+DFS(n-1); 
+		}
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in); 
 		int n = sc.nextInt();
-		System.out.println(T.DFS(n));
+		fibo = new int[n+1]; 
+		for( int i=1; i<=n; i++ ) {
+			System.out.print(T.DFS(i) + " ");
+		}
 	}
 }
