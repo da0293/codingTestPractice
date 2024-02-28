302d1b5d9eaa1457e8037a54b93469eaa7fe16fc
da0293 <dy7133@naver.com>
2024-02-28 23:22:48 +0900

    commit Tree 말단노드까지의 까장 짧은 경로(DFS)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,51 +1,29 @@
 package practice;
 
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+
+class Node{
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		data=val; 
+		lt=rt=null; 
+	}
+}
 
 public class Main {	
-	// 최소 횟수 카운트
-	int answer=0; 
-	// 전진 1칸, 후진 1칸, 전진 5칸 
-	int[] dis= {1, -1, 5}; 
-	// 체크배열, 한번 방문한것은 큐에 넣지않겠다 
-	static int[] ch; 
-	// 갈 수 있는 위치를 저장할 Q 
-	Queue<Integer> Q = new LinkedList<>();
-	public int BFS(int s, int e) {
-		ch=new int[10001]; 
-		// 출발지점 체크
-		ch[s]=1; 
-		Q.offer(s); 
-		// level은 점프횟수와 같다. 
-		int lev=0; 
-		while(!Q.isEmpty()) {
-			int len=Q.size();
-			for(int i=0; i<len; i++) {
-				int x=Q.poll();
-				if(x==e) return lev; 
-				for(int j=0; j<3; j++) {
-					int nx=x+dis[j]; 
-					if(nx>=1 && nx<=10000 && ch[nx]==0) {
-						ch[nx]=1; 
-						Q.offer(nx); 
-					}
-				}
-			}
-			lev++; 
-		}
-		return 0; 
+	Node root;
+	public int DFS(int lev, Node root) {
+		if(root.lt==null && root.rt==null) return lev;
+		else return Math.min(DFS(lev+1, root.lt), DFS(lev+1, root.rt)); 
 	}
 	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		// 현재 위치(현수의 위치)
-		int s=sc.nextInt();
-		// 송아지 위치
-		int e=sc.nextInt();
-		ch=new int[10001]; 
-		// 출발지점 체크
-		System.out.println(T.BFS(s,e));
+		Main t = new Main();
+		t.root=new Node(1);
+		t.root.lt=new Node(2);
+		t.root.rt=new Node(3);
+		t.root.lt.lt=new Node(4);
+		t.root.lt.rt=new Node(5); 
+		System.out.println(t.DFS(0, t.root));
 	}
 }
