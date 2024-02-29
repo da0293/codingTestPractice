82630d033b94f7cff632a9666c85c0c72356e393
da0293 <dy7133@naver.com>
2024-02-29 23:33:49 +0900

    commit 경로탐색(DFS)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,41 +2,40 @@ package practice;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
-class Node{
-	int data;
-	Node lt, rt;
-	public Node(int val) {
-		data=val; 
-		lt=rt=null; 
-	}
-}
 
 public class Main {	
-	Node root;
-	public int BFS(Node root) {
-		Queue<Node> Q = new LinkedList<>();
-		Q.offer(root); 
-		int L=0; 
-		while(!Q.isEmpty()) {
-			int len=Q.size();
-			for( int i=0; i<len; i++ ) {
-				Node current = Q.poll();
-				if(current.lt==null && current.rt==null) return L; 
-				if(current.lt!=null) Q.offer(current.lt); 
-				if(current.rt!=null) Q.offer(current.rt); 
+	static int n,m, answer=0; 
+	static int[][] graph; 
+	static int[] ch; 
+	public void DFS(int v) {
+		if(v==n) answer++; 
+		else {
+			for( int i=1; i<=n; i++ ) {
+				if(graph[v][i]==1 && ch[i]==0) {
+					ch[i]=1; 
+					DFS(i);
+					ch[i]=0; 
+				}
 			}
-			L++; 
 		}
-		return 0; 
 	}
 	public static void main(String[] args) {
 		Main t = new Main();
-		t.root=new Node(1);
-		t.root.lt=new Node(2);
-		t.root.rt=new Node(3);
-		t.root.lt.lt=new Node(4);
-		t.root.lt.rt=new Node(5); 
-		System.out.println(t.BFS(t.root));
+		Scanner sc = new Scanner(System.in); 
+		n=sc.nextInt();
+		m=sc.nextInt();
+		graph=new int[n+1][n+1]; 
+		ch=new int[n+1];
+		ch=new int[n+1]; 
+		for( int i=0; i<m; i++ ) {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			graph[a][b]=1; 
+		}
+		ch[1]=1; 
+		t.DFS(1);
+		System.out.println(answer);
 	}
 }
