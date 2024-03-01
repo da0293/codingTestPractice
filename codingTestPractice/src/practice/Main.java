eb5e98fcb6314767e2736b98f781c378c0b16ee3
da0293 <dy7133@naver.com>
2024-03-01 23:56:30 +0900

    commit 그래프 최단거리(BFS)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -9,17 +9,20 @@ import java.util.Scanner;
 public class Main {	
 	static int n,m, answer=0; 
 	static ArrayList<ArrayList<Integer>> graph; 
-	static int[] ch; 
-	public void DFS(int v) {
-		if(v==n) answer++; 
-		else {
-			// graph.get(v) : v번 ArrayList
-			// ArrayList는 forEach 가능
-			for( int nv : graph.get(v) ) {
+	static int[] ch, dis; 
+	public void BFS(int v) {
+		Queue<Integer> queue = new LinkedList<>();
+		ch[v]=1; 
+		dis[v]=0;
+		queue.offer(v);
+		while(!queue.isEmpty()) {
+			int cv=queue.poll();
+			for(int nv : graph.get(cv)) {
 				if(ch[nv]==0) {
-					ch[nv]=1;
-					DFS(nv);
-					ch[nv]=0; 
+					ch[nv]=1; 
+					queue.offer(nv); 
+					dis[nv]=dis[cv]+1; 
+					
 				}
 			}
 		}
@@ -34,13 +37,15 @@ public class Main {
 			graph.add(new ArrayList<Integer>());
 		}
 		ch=new int[n+1]; 
+		dis=new int[n+1]; 
 		for( int i=0; i<m; i++ ) {
 			int a=sc.nextInt();
 			int b=sc.nextInt();
 			graph.get(a).add(b); 
 		}
-		ch[1]=1; 
-		t.DFS(1);
-		System.out.println(answer);
+		t.BFS(1);
+		for(int i=2; i<=n; i++) {
+			System.out.println(i+" : "+dis[i]);
+		}
 	}
 }
