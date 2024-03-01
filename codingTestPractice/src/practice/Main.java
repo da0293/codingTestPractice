9782ea8f2092c17ce98b2ad25739f60480bb30ca
da0293 <dy7133@naver.com>
2024-03-01 23:51:13 +0900

    commit 경로탐색(인접리스트, ArrayList)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,16 +8,18 @@ import java.util.Scanner;
 
 public class Main {	
 	static int n,m, answer=0; 
-	static int[][] graph; 
+	static ArrayList<ArrayList<Integer>> graph; 
 	static int[] ch; 
 	public void DFS(int v) {
 		if(v==n) answer++; 
 		else {
-			for( int i=1; i<=n; i++ ) {
-				if(graph[v][i]==1 && ch[i]==0) {
-					ch[i]=1; 
-					DFS(i);
-					ch[i]=0; 
+			// graph.get(v) : v번 ArrayList
+			// ArrayList는 forEach 가능
+			for( int nv : graph.get(v) ) {
+				if(ch[nv]==0) {
+					ch[nv]=1;
+					DFS(nv);
+					ch[nv]=0; 
 				}
 			}
 		}
@@ -26,13 +29,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in); 
 		n=sc.nextInt();
 		m=sc.nextInt();
-		graph=new int[n+1][n+1]; 
-		ch=new int[n+1];
+		graph=new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
 		ch=new int[n+1]; 
 		for( int i=0; i<m; i++ ) {
 			int a=sc.nextInt();
 			int b=sc.nextInt();
-			graph[a][b]=1; 
+			graph.get(a).add(b); 
 		}
 		ch[1]=1; 
 		t.DFS(1);
