03169bd109b7a2a43d93b5a8d4ef58e2626960b4
da0293 <dy7133@naver.com>
2024-03-15 23:47:52 +0900

    commit 조합수(메모이제이션)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -4,28 +4,17 @@ import java.util.Scanner;
 
 
 public class Main {	
-	static int n, m, answer=Integer.MAX_VALUE; 
-	public void DFS(int L, int sum, int[] arr ) { 
-		if(sum>m)return; 
-		if(L>=answer)return; 
-		if(sum==m) {
-			answer=Math.min(answer, L);
-		}else {
-			for(int i=0; i<n; i++) {
-				DFS(L+1, sum+arr[i], arr); 
-			}
-		}
+	int[][] dy=new int[35][35];
+	public int DFS(int n, int r) { 
+		if(dy[n][r]>0) return dy[n][r];
+		if(n==r || r==0) return 1; 
+		else return dy[n][r]=DFS(n-1, r-1)+DFS(n-1,r); 
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in); 
-		n=sc.nextInt();
-		int[] arr=new int[n];
-		for( int i=0; i<n; i++ ) {
-			arr[i] = sc.nextInt();
-		}
-		m=sc.nextInt();
-		T.DFS(0, 0, arr);
-		System.out.println(answer);
+		int n=sc.nextInt();
+		int r=sc.nextInt();
+		System.out.println(T.DFS(n, r));
 	}
 }
