2e5fd142850cff9ede9fb0264842a774bf35fb9f
da0293 <dy7133@naver.com>
2024-03-18 02:16:41 +0900

    commit 이진수 출력(재귀)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,20 +1,17 @@
 package practice;
 
-import java.util.Scanner;
-
 
 public class Main {	
-	int[][] dy=new int[35][35];
-	public int DFS(int n, int r) { 
-		if(dy[n][r]>0) return dy[n][r];
-		if(n==r || r==0) return 1; 
-		else return dy[n][r]=DFS(n-1, r-1)+DFS(n-1,r); 
+	public void DFS(int n) {
+		if(n==0)return; 
+		else {
+			DFS(n/2); 
+			System.out.print(n%2 + " ");
+		}
 	}
+
 	public static void main(String[] args) {
 		Main T = new Main();
-		Scanner sc = new Scanner(System.in); 
-		int n=sc.nextInt();
-		int r=sc.nextInt();
-		System.out.println(T.DFS(n, r));
+		T.DFS(11); 
 	}
 }
