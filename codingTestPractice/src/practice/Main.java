f04339146ed4e363f3ae1373f85b0e86fd412d32
da0293 <dy7133@naver.com>
2024-03-24 17:49:12 +0900

    commit 1. 재귀함수

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,45 +1,15 @@
 package practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
-class Node{
-	int data; 
-	Node lt; 
-	Node rt; 
-	public Node(int n) {
-		data=n; 
-		lt=rt=null; 
-	}
-}
+import java.util.Scanner;
 
 public class Main {	
-	Node root; 
-	public void BFS(Node root) {
-		Queue<Node> q = new LinkedList<>();
-		q.offer(root); 
-		int lev = 0; 
-		while(!q.isEmpty()) {
-			int len = q.size();
-			for(int i=0 ;i<len; i++) {
-				Node current = q.poll();
-				System.out.print(current.data + " " );
-				if(current.lt != null) q.offer(current.lt); 
-				if(current.rt != null) q.offer(current.rt); 
-			}
-			lev++; 
-			System.out.println();
-		}
-		
+	public void DFS(int n) {
+		System.out.print(n + " ");
+		if(n==1) return; 
+		DFS(n-1); 
 	}
 	public static void main(String[] args) {
-		Main tree = new Main();
-		tree.root = new Node(1); 
-		tree.root.lt = new Node(2); 
-		tree.root.rt = new Node(3);
-		tree.root.lt.lt = new Node(4); 
-		tree.root.lt.rt = new Node(5);
-		tree.root.rt.lt = new Node(6); 
-		tree.root.rt.rt = new Node(7); 
-		tree.BFS(tree.root); 
+		Main T = new Main();
+		T.DFS(3);
 	}
 }
