39bed1aeb04faa193d4a26e666403c7c3346e730
da0293 <dy7133@naver.com>
2024-03-24 18:07:02 +0900

    commit 피보나치 재귀(메모이제이션)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,16 +2,24 @@ package practice;
 
 import java.util.Scanner;
 
-public class Main {	
-	public void DFS(int n) {
-		if(n==0) return; 
-		DFS(n/2);
-		System.out.print(n%2);
+public class Main {
+	static int n; 
+	static int[] fibo; 
+	public int DFS(int n) {
+		if(fibo[n]>0) return fibo[n]; 
+		if(n==1) return 1; 
+		else if(n==2) return 1; 
+		else return DFS(n-2)+DFS(n-1); 
+		
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		T.DFS(n);
+		fibo = new int[n+1]; 
+		for( int i=1; i<=n; i++ ) {
+			System.out.print(T.DFS(i) + " ");
+		}
+
 	}
 }
