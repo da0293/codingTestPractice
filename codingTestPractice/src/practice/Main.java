59e41aaf668f6be6103dba5e49d123aeb9ca5f84
da0293 <dy7133@naver.com>
2024-03-24 18:34:00 +0900

    commit 5. 이진트리순회(DFS : Depth-First Search)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -2,24 +2,35 @@ package practice;
 
 import java.util.Scanner;
 
+class Node{
+	int data;
+	Node lt; 
+	Node rt;
+	
+	public Node(int n) {
+		data = n; 
+		lt = rt = null; 
+	}
+}
+
 public class Main {
-	static int n; 
-	static int[] fibo; 
-	public int DFS(int n) {
-		if(fibo[n]>0) return fibo[n]; 
-		if(n==1) return 1; 
-		else if(n==2) return 1; 
-		else return DFS(n-2)+DFS(n-1); 
+	Node root; 
+	public void DFS(Node n) {
+		if(n==null)return; 
+		DFS(n.lt); 
+		DFS(n.rt);
+		System.out.print(n.data + " ");
 		
 	}
 	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		fibo = new int[n+1]; 
-		for( int i=1; i<=n; i++ ) {
-			System.out.print(T.DFS(i) + " ");
-		}
-
+		Main Tree = new Main();
+		Tree.root = new Node(1); 
+		Tree.root.lt = new Node(2); 
+		Tree.root.rt = new Node(3); 
+		Tree.root.lt.lt = new Node(4); 
+		Tree.root.lt.rt = new Node(5); 
+		Tree.root.rt.lt = new Node(6); 
+		Tree.root.rt.rt = new Node(7); 
+		Tree.DFS(Tree.root); 
 	}
 }
