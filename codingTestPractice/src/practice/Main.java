c2c04acf1709ae2f61188bd85a6e22e0fece885e
da0293 <dy7133@naver.com>
2024-03-27 22:01:27 +0900

    commit 경로탐색(인접 행렬)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,53 +1,42 @@
 package practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
-
 public class Main {
-	int[] dis = {-1,1,5}; 
-	int[] ck; 
-	public int BFS(int s, int e) {
-		ck = new int[10001]; 
-		ck[s] = 1; 
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(s);
-		int lev=0;
-		while(!q.isEmpty()) {
-			int len = q.size();
-			// 해당 레벨 for문 돌기
-			for(int i=0; i<len; i++) {
-				// 큐에서 좌표 꺼냄 
-				int x = q.poll();
-				// 한 좌표당 앞으로 1, 뒤로 -1, 앞으로 5 즉 3가지 경우가 있다. 
-				for(int j=0; j<3; j++) {
-					// 해당좌표에 경우의 수 더함 
-					int nx = x+dis[j]; 
-					// 그 레벨이 거리값과 같다. 그래서 송아지좌표와 같다면 lev 리턴
-					if(nx==e) return lev++; 
-					// 좌표의 범위가 1~100000이므로 이것 지킴 
-					// 그리고 해당 좌표(nx)가 아직 방문되지 않았을 때 큐에 추가 
-					if(nx >=1 && nx<=10000 && ck[nx]==0) {
-						ck[nx]=1; 
-						q.offer(nx); 
-					}
-					
-				}
-			}
-			lev++; 
-		}
-		return 0; 
-		
-	}
-	public static void main(String[] args) {
-		Main T = new Main();
-		Scanner sc = new Scanner(System.in); 
-		// 현수 위치
-		int s = sc.nextInt();
-		// 송아지 위치
-		int e = sc.nextInt();
-		System.out.println(T.BFS(s,e));
-		
-	}
+    static int n, m, answer = 0; // 정점의 개수, 간선의 개수, 경로의 가지 수
+    static int[][] graph; // 인접 행렬
+    static int[] ck; // 방문 여부 체크 배열
+
+    // DFS 함수: 주어진 그래프에서 모든 경로를 탐색하여 가지 수를 카운트하는 메서드
+    public int DFS(int x) {
+    	// 목표 정점에 도달한 경우
+        if (x == n) answer++; // 경로의 가지 수 증가
+        else {
+            for (int i = 1; i <= n; i++) {
+                if (graph[x][i] == 1 && ck[i] == 0) { // 연결되어 있고 방문하지 않은 정점인 경우
+                    ck[i] = 1; // 해당 정점 방문 체크
+                    DFS(i); // 다음 정점으로 DFS 호출
+                    ck[i] = 0; // 백트래킹: 해당 정점 방문 해제
+                }
+            }
+        }
+        return 0; // 재귀 종료
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); // 정점의 개수 입력
+        m = sc.nextInt(); // 간선의 개수 입력
+        ck = new int[n + 1]; // 방문 여부 체크 배열 초기화
+        graph = new int[n + 1][n + 1]; // 인접 행렬 초기화
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt(); // 간선의 시작 정점
+            int b = sc.nextInt(); // 간선의 도착 정점
+            graph[a][b] = 1; // 그래프에 간선 추가
+        }
+        ck[1] = 1; // 시작 정점 체크
+        T.DFS(1); // DFS 호출
+        System.out.println(answer); // 결과 출력
+    }
 }
