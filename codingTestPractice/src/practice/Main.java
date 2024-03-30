5a2b098222462af2db023ee32cbedf5c608ef8c0
da0293 <dy7133@naver.com>
2024-03-30 22:16:06 +0900

    commit 그래프 최단거리(BFS)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,44 +1,57 @@
 package practice;
 
+
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int n, m, answer = 0; // 정점의 개수, 간선의 개수, 경로의 가지 수
-    static ArrayList<ArrayList<Integer>> graph; 
-    static int[] ck; // 방문 여부 체크 배열
+	static int n, m; 
+	static int[] ck; 
+	static int[] dis; 
+	static ArrayList<ArrayList<Integer>> graph; 
+	
+	public void BFS(int start) {
+		Queue<Integer> q = new LinkedList<>(); 
+		ck[start]=1; 
+		dis[start]=0; 
+		q.offer(start); 
+		while(!q.isEmpty()) {
+			int current = q.poll(); 
+			for( int next : graph.get(current)) {
+				if(ck[next]==0) {
+					ck[next]=1; 
+					dis[next]=dis[current]+1; 
+					q.offer(next); 
+				}
+			}
+			
+			
+		}
+		
+	}
 
-    // DFS 함수: 주어진 그래프에서 모든 경로를 탐색하여 가지 수를 카운트하는 메서드
-    public void DFS(int x) {
-    	if(x==n) answer++; 
-    	else {
-    		for(int nv : graph.get(x)) {
-    			if(ck[nv]==0) {
-    				ck[nv]=1; 
-    				DFS(nv);
-    				ck[nv]=0; 
- 
-    			}
-    		}
-    	}
-    	
-    }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); //정점의 개수 입력
-        m = sc.nextInt(); //간선의 개수 입력
-        ck = new int[n + 1]; //방문 여부 체크 배열 초기화
-        graph = new ArrayList<>(); 
-        //인접 리스트 인덱스 리스트가 노드가 되고, 인덱스 리스트 안에 있는 Integer들이 노드에서 갈 수 있는 접점들이다. 
-        for(int i=0; i<m; i++) {
-        	int a = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ck = new int[n+1]; 
+        dis = new int[n+1]; 
+        graph = new ArrayList<ArrayList<Integer>>(); 
+        for(int i=0; i<=n; i++) {
+        	graph.add(new ArrayList<Integer>()); 
+        }
+        for( int i=0; i<m; i++ ) {
+        	int a = sc.nextInt(); 
         	int b = sc.nextInt(); 
         	graph.get(a).add(b); 
         }
-        ck[1]=1; 
-        T.DFS(1); 
-        System.out.println(answer); // 결과 출력
+        T.BFS(1); 
+        for(int i=2; i<=n; i++) {
+        	System.out.println(i + " : " +dis[i]);
+        }
     }
 }
