49b167e7a4c5f56ecfff0b0405dc998e989f1732
da0293 <dy7133@naver.com>
2024-04-01 01:54:19 +0900

    commit 피보나치 수열

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -3,13 +3,13 @@ package practice;
 import java.util.Scanner;
 
 public class Main {
+	static int[] fibo; 
 	
-	public int DFS(int n) { // 11(5, 1) 5(2,1) 2(2
-		if(n==1) return 1;  
-		else {
-			return n*DFS(n-1); 
-		}
-		
+	public int DFS(int n) {
+		if(fibo[n]>0) return fibo[n]; 
+		if(n==1) return fibo[n]=1;   
+		else if(n==2) return fibo[n]=1;  
+		else return fibo[n]=DFS(n-2)+DFS(n-1);
 	}
 
 
@@ -17,7 +17,10 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); 
-        System.out.println(T.DFS(n));
-       
+        fibo = new int [n+1]; 
+        for(int i=1; i<=n; i++) {
+        	System.out.print(T.DFS(i)+ " ");
+        }
+               
     }
 }
