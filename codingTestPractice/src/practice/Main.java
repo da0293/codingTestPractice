4d35beeafea3e71df756ff44ee28bfd3d2dd5858
da0293 <dy7133@naver.com>
2024-04-01 01:34:01 +0900

    commit 이진수 출력(재귀)

--- codingTestPractice/src/practice/Main.java
+++ codingTestPractice/src/practice/Main.java
@@ -1,33 +1,14 @@
 package practice;
 
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static int n, m; 
-	static int[] ck; 
-	static int[] dis; 
-	static ArrayList<ArrayList<Integer>> graph; 
 	
-	public void BFS(int start) {
-		Queue<Integer> q = new LinkedList<>(); 
-		ck[start]=1; 
-		dis[start]=0; 
-		q.offer(start); 
-		while(!q.isEmpty()) {
-			int current = q.poll(); 
-			for( int next : graph.get(current)) {
-				if(ck[next]==0) {
-					ck[next]=1; 
-					dis[next]=dis[current]+1; 
-					q.offer(next); 
-				}
-			}
-			
-			
+	public void DFS(int n) { // 11(5, 1) 5(2,1) 2(2
+		if(n==0) return; 
+		else {
+			DFS(n/2); 
+			System.out.print(n%2);
 		}
 		
 	}
@@ -36,22 +17,8 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        ck = new int[n+1]; 
-        dis = new int[n+1]; 
-        graph = new ArrayList<ArrayList<Integer>>(); 
-        for(int i=0; i<=n; i++) {
-        	graph.add(new ArrayList<Integer>()); 
-        }
-        for( int i=0; i<m; i++ ) {
-        	int a = sc.nextInt(); 
-        	int b = sc.nextInt(); 
-        	graph.get(a).add(b); 
-        }
-        T.BFS(1); 
-        for(int i=2; i<=n; i++) {
-        	System.out.println(i + " : " +dis[i]);
-        }
+        int n = sc.nextInt(); 
+        T.DFS(n);
+       
     }
 }
